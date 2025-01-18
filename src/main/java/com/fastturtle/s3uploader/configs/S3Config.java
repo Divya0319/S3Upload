52df267e43af6f2d191c9017891c30979d74dbde
divya0319 <divygupta0319@gmail.com>
2025-01-18 22:10:04 +0530

    Added Spring cloud starter aws, and awssdk s3 dependency, added S3client dependency, and started with S3Service

---
+++ src/main/java/com/fastturtle/s3uploader/configs/S3Config.java
@@ -0,0 +1,19 @@
+package com.fastturtle.s3uploader.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of("ap-northeast-1"))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+}
