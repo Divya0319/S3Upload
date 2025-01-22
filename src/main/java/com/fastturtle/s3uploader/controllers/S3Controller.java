675cb8fe67dbcdfa19644cd551de1f52ac17e753
divya0319 <divygupta0319@gmail.com>
2025-01-22 22:31:34 +0530

    Added multipart upload changes to controllers

--- src/main/java/com/fastturtle/s3uploader/controllers/S3Controller.java
+++ src/main/java/com/fastturtle/s3uploader/controllers/S3Controller.java
@@ -1,11 +1,13 @@
 package com.fastturtle.s3uploader.controllers;
 
+import com.fastturtle.s3uploader.services.S3MultipartUpload;
 import com.fastturtle.s3uploader.utils.FileRequest;
 import com.fastturtle.s3uploader.services.S3Service;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 @RestController
@@ -14,16 +16,33 @@ public class S3Controller {
 
     private final S3Service s3Service;
 
-    public S3Controller(S3Service s3Service) {
+    private final S3MultipartUpload s3MultipartUpload;
+
+    public S3Controller(S3Service s3Service, S3MultipartUpload s3MultipartUpload) {
         this.s3Service = s3Service;
+        this.s3MultipartUpload = s3MultipartUpload;
     }
 
     @PostMapping("/upload")
-    public Map<String, String> uploadFile(@RequestParam("file") File file) {
-        String bucketName = "bucket-for-expenses-csv";
-        String fileName = file.getName();
+    public Map<String, String> uploadFile(@RequestParam("file") MultipartFile multipartFile) {
 
-        String fileUrl = s3Service.uploadFile(bucketName, fileName, file);
+        File tempFile;
+        String fileName = multipartFile.getOriginalFilename();
+        try {
+            tempFile = File.createTempFile("upload-", multipartFile.getOriginalFilename());
+            multipartFile.transferTo(tempFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String bucketName = "bucket-for-expenses-csv";
+        String fileUrl;
+
+        if(multipartFile.getSize() <= 1024 * 1024) {
+            fileUrl = s3Service.uploadFile(bucketName, fileName, tempFile);
+        } else {
+            fileUrl = s3MultipartUpload.multipartUpload(bucketName, fileName, tempFile);
+        }
         return Map.of("fileUrl", fileUrl);
     }
 
--- src/main/java/com/fastturtle/s3uploader/controllers/S3UploaderController.java
+++ src/main/java/com/fastturtle/s3uploader/controllers/S3UploaderController.java
@@ -1,5 +1,6 @@
 package com.fastturtle.s3uploader.controllers;
 
+import com.fastturtle.s3uploader.services.S3MultipartUpload;
 import com.fastturtle.s3uploader.services.S3Service;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,8 +17,11 @@ public class S3UploaderController {
 
     private final S3Service s3Service;
 
-    public S3UploaderController(S3Service s3Service) {
+    private final S3MultipartUpload s3MultipartUpload;
+
+    public S3UploaderController(S3Service s3Service, S3MultipartUpload s3MultipartUpload) {
         this.s3Service = s3Service;
+        this.s3MultipartUpload = s3MultipartUpload;
     }
 
     @GetMapping("/upload")
@@ -26,11 +30,26 @@ public class S3UploaderController {
     }
 
     @PostMapping("/upload")
-    public String uploadFile(@RequestParam("file") File file, Model model) {
-        String bucketName = "bucket-for-expenses-csv";
-        String fileName = file.getName();
+    public String uploadFile(@RequestParam("file") MultipartFile multipartFile, Model model) {
 
-        String uploadFileUrl = s3Service.uploadFile(bucketName, fileName, file);
+        File tempFile;
+        String fileName = multipartFile.getOriginalFilename();
+        try {
+            tempFile = File.createTempFile("upload-", multipartFile.getOriginalFilename());
+            multipartFile.transferTo(tempFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String bucketName = "bucket-for-expenses-csv";
+
+        String uploadFileUrl;
+
+        if(multipartFile.getSize() <= 1024 * 1024) {
+            uploadFileUrl = s3Service.uploadFile(bucketName, fileName, tempFile);
+        } else {
+            uploadFileUrl = s3MultipartUpload.multipartUpload(bucketName, fileName, tempFile);
+        }
 
         String fileType = null;
 
